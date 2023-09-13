1f15b7908becfee7a4e20d70a849a784eb501a83
Sabithasan2098 <sabithasan2098@gmail.com>
2023-09-13 12:14:38 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
  
 import './App.css'
+import Header from './component/header/Header'
+import Blogs from './components/Blogs'
 
 function App() {
    
 
   return (
     <>  
-      <h1 className='text-6xl bg-yellow-300  text-green-500 rounded-full mt-10 font-bold text-center p-10 mx-96'>React Cafe knowledge</h1>
-       
+     <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
