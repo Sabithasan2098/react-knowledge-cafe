394bd9f342d3102c515831b95c833d442ead9b5d
Sabithasan2098 <sabithasan2098@gmail.com>
2023-09-13 11:11:16 +0600

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>  
+      <h1 className='text-6xl bg-yellow-300  text-green-500 rounded-full mt-10 font-bold text-center p-10 mx-96'>React Cafe knowledge</h1>
+       
+    </>
+  )
+}
+
+export default App
